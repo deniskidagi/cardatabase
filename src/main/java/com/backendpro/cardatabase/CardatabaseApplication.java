9e59aaca2e85fac3c6e015d5be7a8d3a089a9b34
deniskidagi <deniskidagi@gmail.com>
2024-06-30 22:39:07 +0300

    created owner and car repositories

--- src/main/java/com/backendpro/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/backendpro/cardatabase/CardatabaseApplication.java
@@ -1,6 +1,9 @@
 package com.backendpro.cardatabase;
 
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.backendpro.cardatabase.domain.Car;
 import com.backendpro.cardatabase.domain.CarRepository;
+import com.backendpro.cardatabase.domain.Owner;
+import com.backendpro.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner {
@@ -15,10 +20,14 @@ public class CardatabaseApplication implements CommandLineRunner {
 			LoggerFactory.getLogger(CardatabaseApplication.class);
 	
 	private final CarRepository repository;
+	private final OwnerRepository orepository;
 	
-	public CardatabaseApplication(CarRepository repository) {
+	public CardatabaseApplication(CarRepository repository, OwnerRepository orepository) {
 		this.repository = repository;
+		this.orepository = orepository;
 	}
+	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 		
@@ -28,10 +37,15 @@ public class CardatabaseApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.save(new Car("Ford", "Mustang", "Red", "ADF 1121", 2023, 29000));
-		repository.save(new Car("Audi", "Rs8", "Red", "ADF 1121", 2024, 99000));
-		repository.save(new Car("Toyota", "Land Cruiser", "Black", "ADF 1121", 2021, 50000));
-		repository.save(new Car("Nissan", "GT R", "red", "ADF 1121", 2019, 29000));
+		Owner owner1 = new Owner("John", "Johnson");
+		Owner owner2 = new Owner("Mary", "Robinson");
+		orepository.saveAll(Arrays.asList(owner1, owner2));
+		
+		
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF 1121", 2023, 29000, owner1));
+		repository.save(new Car("Audi", "Rs8", "Red", "ADF 1121", 2024, 99000, owner2));
+		repository.save(new Car("Toyota", "Land Cruiser", "Black", "ADF 1121", 2021, 50000, owner1));
+		repository.save(new Car("Nissan", "GT R", "red", "ADF 1121", 2019, 29000, owner2));
 		
 		for(Car car: repository.findAll()) {
 			logger.info("brand: {}, model: {}", car.getBrand(), car.getModel());
--- src/main/java/com/backendpro/cardatabase/domain/Car.java
+++ src/main/java/com/backendpro/cardatabase/domain/Car.java
@@ -1,9 +1,14 @@
 package com.backendpro.cardatabase.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
 
 @Entity
 public class Car {
@@ -13,11 +18,23 @@ public class Car {
 	private String brand, model, color, registrationNumber;
 	private int modelYear, price;
 	
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="owner")
+	private Owner owner;
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
 	public Car() {
 		
 	}
 
-	public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price) {
+	public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price, Owner owner) {
 		super();
 		this.brand = brand;
 		this.model = model;
@@ -25,6 +42,7 @@ public class Car {
 		this.registrationNumber = registrationNumber;
 		this.modelYear = modelYear;
 		this.price = price;
+		this.owner = owner;
 	}
 
 	public String getBrand() {
@@ -74,5 +92,6 @@ public class Car {
 	public void setPrice(int price) {
 		this.price = price;
 	}
+	
 	
 }
